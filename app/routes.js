c1206635e27a8ebbbe301ca813bd96a4fc0305c8
Daniel <danielyochumjr@gmail.com>
2017-01-06 12:02:57 -0500

    beers route functioning

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,11 @@
 
 const url = "http://api.brewerydb.com/v2/";
 const BREWERYDB_KEY = process.env.BREWERYDB_KEY;
-const apiKey = `&key=${BREWERYDB_KEY}`;
 const request = require('request');
 
 
 module.exports = function(app){
+
   app.post('/locations', function(req,res){ //Receive location info and send list
     console.log("POST to /locations recieved with data: ", req.body);
     if(!req.body.location){
@@ -14,6 +14,7 @@ module.exports = function(app){
     }
     const paramQuery = `search/geo/point?lat=${req.body.location.lat}&lng=${req.body.location.lng}`;
     const radius = `&radius=${req.body.radius}`
+    const apiKey = `&key=${BREWERYDB_KEY}`;
     const fullQuery = url + paramQuery + radius + apiKey;
     console.log("THIS IS REQ.BODY", req.body);
     request({
@@ -24,16 +25,17 @@ module.exports = function(app){
           console.log("ERROR!", error);
           response.json("error");
         } else if (response) {
-            console.log(body);
+            console.log("Request completed and sent body");
             res.send(body);
         }
       }
-    }); //End locations request
-  }); //End locations POST
+    }); //End /locations request
+  }); //End /locations POST
 
   app.post('/beers', function(req, res){
-    console.log("POST to /beers recieved with data: ", req.body);
-    const paramQuery = `brewery/${req.body.locationId}/beers`;
+    console.log("POST to /beers recieved with data: ", req.body.brewery);
+    const paramQuery = `brewery/${req.body.brewery}/beers`;
+    const apiKey = `?key=${BREWERYDB_KEY}`;
     const fullQuery = url + paramQuery + apiKey;
     request({
       url: fullQuery,
@@ -47,7 +49,7 @@ module.exports = function(app){
             res.send(body);
         }
       }
-    })//End beers request
-  })//End beers POST
+    })//End /beers request
+  })//End /beers POST
 
 }
